1e3f6ab4c94d0ffc6e28b9b6a467e6de89fb9a2a
SirionFaust <faustoi96@gmail.com>
2026-03-14 23:33:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function showCopySuccess(element) {
     element.classList.remove("fas", "fa-check");
     element.classList.add("far", "fa-copy");
     element.style.color = "";
-  }, 1500);
+  }, 1000);
 }
 
 function generatePalette() {
